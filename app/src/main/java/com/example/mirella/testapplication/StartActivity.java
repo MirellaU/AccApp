0453643aa77057ac3593bdee90ada154d9ea30dd
MirellaU <mireurz373@student.polsl.pl>
2019-11-14 10:20:56 +0100

    change saved data format

--- app/src/main/java/com/example/mirella/testapplication/StartActivity.java
+++ app/src/main/java/com/example/mirella/testapplication/StartActivity.java
@@ -110,10 +110,12 @@ public class StartActivity extends AppCompatActivity {
         String csv = android.os.Environment.getExternalStorageDirectory().getAbsolutePath();
         try {
             CSVWriter writer = new CSVWriter(new FileWriter(csv));
-            List<String[]> data = new ArrayList<String[]>();
-            data.add(new String[] {"India", "New Delhi"});
-            data.add(new String[] {"United States", "Washington D.C"});
-            data.add(new String[] {"Germany", "Berlin"});
+            ArrayList data = new ArrayList<>();
+            data.add(timeStamp);
+            data.add("Roll: ");
+            data.add(rollValues);
+            data.add("Pitch: ");
+            data.add(pitchValues);
 
             writer.writeAll(data);
             writer.close();
